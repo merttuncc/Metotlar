4abe3134e6f6d348a6863997dac4dd5b43af5ac1
Turkuvaz <Turkuvaz@DESKTOP-OJ1NE1U>
2021-09-30 11:58:22 +0300

    Add project files.

---
+++ Metotlar/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Money { get; set; }
+        public string  Description { get; set; }
+    }
+}
